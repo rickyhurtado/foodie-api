3903edd41f65018ab3fe1e0e08dbddd5670a7027
Ricky Hurtado <odatruh.ykcir@gmail.com>
2016-08-28 11:35:00 +0800

    Add is_admin? and is_editor? to User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,5 +1,18 @@
+require 'test_helper'
+
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @admin = User.first
+    @editor = User.second
+  end
+
+  test '#is_admin?' do
+    assert @admin.is_admin?
+    assert_not @editor.is_admin?
+  end
+
+  test '#is_editor?' do
+    assert @admin.is_editor?
+    assert @editor.is_editor?
+  end
 end
