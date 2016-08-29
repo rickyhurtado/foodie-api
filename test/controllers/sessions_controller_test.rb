4b8c33c2395ec11b029a6ba94eb7d302bb6ca96a
Ricky Hurtado <odatruh.ykcir@gmail.com>
2016-08-29 18:40:51 +0800

    Update the sessions controller test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,14 +1,23 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user_params = { user: {
+      email: 'admin+user@example.org',
+      password: 'passw0rd' }
+    }
+  end
+
   test "should respond 201 for valid email and password" do
-    post user_session_url, params: { user: { email: 'admin+user@example.org', password: 'passw0rd' } }, as: :json
+    post user_session_url, params: @user_params, as: :json
 
     assert_response 201
   end
 
   test "should respond 401 for invalid email or password" do
-    post user_session_url, params: { user: { email: 'admin+user@example.org', password: 'password' } }, as: :json
+    @user_params[:user][:password] = 'wrongpassword'
+
+    post user_session_url, params: @user_params, as: :json
 
     assert_response 401
   end
