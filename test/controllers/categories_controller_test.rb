f5364c9aeaa74c8cfefe89b28d1315a887cf83cb
Ricky Hurtado <odatruh.ykcir@gmail.com>
2016-08-29 18:38:05 +0800

    Update the categories controller test

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -5,34 +5,35 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = User.first
     @category = Category.first
+    @user_header_params = { AUTHORIZATION: @user.authentication_token }
   end
 
   test "should get index" do
-    get categories_url, headers: { AUTHORIZATION: @user.authentication_token }, as: :json
+    get categories_url, headers: @user_header_params, as: :json
     assert_response :success
   end
 
   test "should create category" do
     assert_difference('Category.count') do
-      post categories_url, params: { category: { name: 'New Category' } }, headers: { AUTHORIZATION: @user.authentication_token }, as: :json
+      post categories_url, params: { category: { name: 'New Category' } }, headers: @user_header_params, as: :json
     end
 
     assert_response 201
   end
 
   test "should show category" do
-    get category_url(@category), headers: { AUTHORIZATION: @user.authentication_token }, as: :json
+    get category_url(@category), headers: @user_header_params, as: :json
     assert_response :success
   end
 
   test "should update category" do
-    patch category_url(@category), params: { category: { name: @category.name } }, headers: { AUTHORIZATION: @user.authentication_token }, as: :json
+    patch category_url(@category), params: { category: { name: @category.name } }, headers: @user_header_params, as: :json
     assert_response 200
   end
 
   test "should destroy category" do
     assert_difference('Category.count', -1) do
-      delete category_url(@category), headers: { AUTHORIZATION: @user.authentication_token }, as: :json
+      delete category_url(@category), headers: @user_header_params, as: :json
     end
 
     assert_response 204
