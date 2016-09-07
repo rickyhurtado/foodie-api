a309defcbcddc44743df9b6b9d61fe2f49bf7aec
Ricky Hurtado <odatruh.ykcir@gmail.com>
2016-09-07 10:20:46 +0800

    Update seeds and Kaminari config

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,20 +8,30 @@ post = Category.create(name: 'Post')
 recipe = Category.create(name: 'Recipe')
 review = Category.create(name: 'Review')
 
-Blog.create(title: 'First Blog: Post', body: '<p>This is first blog post body.</p>', status: 'published', category: post, user: blog_user_1, published_at: 70.days.ago)
-Blog.create(title: 'First Blog: Recipe', body: '<p>This is first blog recipe body.</p>', status: 'published', category: recipe, user: blog_user_1, published_at: 35.days.ago)
-Blog.create(title: 'First Blog: Review', body: '<p>This is first blog review body.</p>', status: 'published', category: review, user: blog_user_1, published_at: 20.days.ago)
+sample_text = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at ligula leo. Ut facilisis tellus eget semper volutpat. Fusce ac magna pretium augue pharetra tempus ut at dui.
+                  Pellentesque vestibulum odio ut metus cursus, id ullamcorper augue volutpat. Nullam facilisis dui ut metus rhoncus, imperdiet faucibus turpis posuere. Sed tempor vestibulum dolor
+                  in auctor.</p><p>Aenean bibendum aliquet velit, nec ultrices dui. Nullam sed enim magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                  Phasellus in enim tellus. Donec quis elit nec sapien eleifend tempus vel et purus. Integer faucibus, lectus et interdum scelerisque, magna est suscipit neque, vitae rutrum neque
+                  felis convallis velit. Nunc sed magna justo. Sed quis lorem et ligula congue ultricies eu at ipsum. In vitae libero nec ligula efficitur viverra at nec nisi. Cum sociis natoque
+                  penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>'
 
-Blog.create(title: 'Second Blog: Post', body: '<p>This is second blog post body.</p>', status: 'published' , category: post, user: blog_user_2, published_at: 18.days.ago)
-Blog.create(title: 'Second Blog: Recipe', body: '<p>This is second blog recipe body.</p>', status: 'published', category: recipe, user: blog_user_2, published_at: 10.days.ago)
-Blog.create(title: 'Second Blog: Review', body: '<p>This is second blog review body.</p>', status: 'draft', category: review, user: blog_user_2, published_at: 7.days.ago)
+Blog.create(title: 'First Blog: Post', body: sample_text, status: 'published', category: post, user: blog_user_1, published_at: 70.days.ago)
+Blog.create(title: 'First Blog: Recipe', body: sample_text, status: 'published', category: recipe, user: blog_user_1, published_at: 35.days.ago)
+Blog.create(title: 'First Blog: Review', body: sample_text, status: 'published', category: review, user: blog_user_1, published_at: 27.days.ago)
+Blog.create(title: 'First Blog: Another Post', body: sample_text, status: 'published', category: post, user: blog_user_1, published_at: 20.days.ago)
 
-Blog.create(title: 'Third Blog: Post', body: '<p>This is third blog post body.</p>', status: 'published', category: post, user: blog_user_3, published_at: 3.days.ago)
-Blog.create(title: 'Third Blog: Recipe', body: '<p>This is third blog recipe body.</p>', status: 'draft', category: recipe, user: blog_user_3, published_at: 7.hours.ago)
-Blog.create(title: 'Third Blog: Review', body: '<p>This is third blog review body.</p>', status: 'draft', category: review, user: blog_user_3)
+Blog.create(title: 'Second Blog: Post', body: sample_text, status: 'published' , category: post, user: blog_user_2, published_at: 18.days.ago)
+Blog.create(title: 'Second Blog: Recipe', body: sample_text, status: 'published', category: recipe, user: blog_user_2, published_at: 10.days.ago)
+Blog.create(title: 'Second Blog: Review', body: sample_text, status: 'draft', category: review, user: blog_user_2, published_at: 7.days.ago)
+Blog.create(title: 'Second Blog: Another Recipe', body: sample_text, status: 'published', category: recipe, user: blog_user_2, published_at: 5.days.ago)
+
+Blog.create(title: 'Third Blog: Post', body: sample_text, status: 'published', category: post, user: blog_user_3, published_at: 3.days.ago)
+Blog.create(title: 'Third Blog: Recipe', body: sample_text, status: 'draft', category: recipe, user: blog_user_3, published_at: 7.hours.ago)
+Blog.create(title: 'Third Blog: Review', body: sample_text, status: 'draft', category: review, user: blog_user_3)
+Blog.create(title: 'Third Blog: Another Review', body: sample_text, status: 'published', category: review, user: blog_user_3, published_at: 5.minutes.ago)
 
 puts '[Create blog to test the activity stream]'
-blog_activity = Blog.create(title: 'Activity Blog: Post', body: '<p>This blog post tests the activity actions.</p>', status: 'draft', category: post, user: blog_user_1)
+blog_activity = Blog.create(title: 'Activity Blog: Post', body: sample_text, status: 'draft', category: post, user: blog_user_1)
 
 puts '[Update blog to test the activity stream]'
 Blog.find(blog_activity).update_attributes(title: 'Activity Blog: Post [updated]', status: 'draft')
